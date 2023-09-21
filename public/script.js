5ee61ed48ef69b0397a92aabc32e4f7929261f6b
Iamneo <techadmin@iamneo.ai>
2023-09-21 07:16:28 +0000

    Backup Commit

--- public/script.js
+++ public/script.js
@@ -1,3 +1,31 @@
 function validateForm(event) {
-  // Write your javascript validation code here
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+  const messageInput = document.getElementById("message");
+
+  let valid = true;
+
+  if (nameInput.value.trim() === "") {
+    valid = false;
+    alert("Please enter your name.");
+  }
+
+  if (!isValidEmail(emailInput.value.trim())) {
+    valid = false;
+    alert("Please enter a valid email address.");
+  }
+
+  if (messageInput.value.trim() === "") {
+    valid = false;
+    alert("Please enter a message.");
+  }
+
+  if (!valid) {
+    event.preventDefault();
+  }
+}
+
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
 }
